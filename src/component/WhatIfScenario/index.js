ea27675501a78c2e18bf9a1d3d4f85b215836117
balajiparthiban <balaji.parthiban@latentview.com>
2023-04-11 13:49:07 +0530

    what if scenario page

--- src/component/WhatIfScenario/index.js
+++ src/component/WhatIfScenario/index.js
@@ -1,9 +1,25 @@
-
+import './style.scss'
 
 const WhatIfScenario = () => {
+  const demos = {
+    soundcloud:
+      '<iframe width="100%" height="800" scrolling="no" frameborder="no" allow="autoplay" src="https://app.powerbi.com/view?r=eyJrIjoiNzY5NDA5ZTctM2NmMS00YjM0LWFhODgtMWNkZGM0OGI3ZWI5IiwidCI6IjFjMmJlMTE2LWQyMDctNDJmMy05NTJiLWM2NjI4OTc0NTg3OSIsImMiOjZ9"></iframe>',
+  };
+
+  function Iframe(props) {
     return (
-        <div>What if scenario page</div>
-    )
+      <div
+        dangerouslySetInnerHTML={{ __html: props.iframe ? props.iframe : "" }}
+      />
+    );
+  }
+
+  return (
+    <div className="what-if-scenario">
+      <h3>What If Scenario</h3>
+      <Iframe iframe={demos["soundcloud"]} allow="autoplay" />
+    </div>
+  );
 }
 
-export default WhatIfScenario
+export default WhatIfScenario;
